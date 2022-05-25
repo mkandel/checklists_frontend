530b8dc6356287026a55e829956ee3862ce9e03a
Marc Kandel <Marc.Kandel@five9.com>
2022-05-25 17:18:47 -0400

    WIP

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
+import { createRouter } from "vue-router";
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
+const router = createRouter();
+app.use(router);
 app.mount("#app");
